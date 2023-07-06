5546945a71b0443ada1e82dfc04c5e2e6ef604f9
akira-shirakawa <akun32310901@yahoo.co.jp>
2023-07-07 03:03:44 +0900

    first_migration

---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    @include('common.head')
+    <title>Document</title>
+
+</head>
+<body>
+@include('layouts.header')
+<div class="columns">
+    <div class="column"></div>
+    <div class="column">
+    <section class="section is-large">
+    <h1 class="title">Large section</h1>
+    <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading.
+    </h2>
+    </section>
+    </div>
+    <div class="column"></div>
+</div>
+</body>
+</html>
