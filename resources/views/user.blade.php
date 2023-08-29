1f776522c65fa40c2596e6f06cda841b3f3db55a
akira-shirakawa <akun32310901@yahoo.co.jp>
2023-08-29 12:43:25 +0900

    changed

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -41,21 +41,13 @@
                 自分の投稿
             </h2>
             @foreach($articles as $article)
-            <!-- <a class="item" href="{{route('articles.show',['article'=>$article])}}">
+            <a class="item" href="{{route('articles.show',['article'=>$article])}}">
                 <div class="item_left">
                 
                     <img src="{{asset('category_images/category_'.$article->category.'.png')}}" alt="" width="60px" height="60px">
                 </div>
-                <div class="item_right">
-                <div class="item_title">{{$article->title}}</div>
-                <div class="item_bottom">{{$article->created_at->diffForHumans()}} &emsp;<i class="fa-regular fa-comment"></i>{{$article->comments->count()}} &emsp;投票数:{{$article->votes()->count()}}&emsp;
-                <form action="{{route('articles.destroy',['article'=>$article])}}" method="post">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-danger">削除</button>
-                </div>
-                </div>
-            </a> -->
+                
+            </a>
             @endforeach
         </div>
     </div>
